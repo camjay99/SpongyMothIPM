69bb289478d7a1a9d389c26555369c62019a38f9
Scholl <406260@win.lanl.gov>
2025-07-09 15:55:32 -0600

    Change LnormCDF to autograd function
    Changed LnormCDF to implement the torch.autograd.Function interface.
    Previously, autograd would run into numerical instability problems when
    computing gradients through LnormCDF. By manually implementing x and
    sigma derivatives, we can write the results in ways that are more
    numerically stable to compute.

--- SpongyMothIPM/util.py
+++ SpongyMothIPM/util.py
@@ -2,36 +2,75 @@ import math
 
 import torch
 
-def LnormPDF(x, mu, sigma):
-    """Compute the log-normal pdf based on x and a tensor of mu/sigma."""
-    # When x = 0, lognormal pdf will return nan when it should be 0.
-    # Therefore, we slightly nudge values first (this helps ensure
-    # differentiability, which zeroing the first entry would not do).
-    # When mu = 0, set 1 along the diagonal and rest of value in column
-    # to 0.
-    x = x + 0.000000001
-    dist = (1
+# Automatic differentiaion of the log normal cdf is not numerically stable.
+# Here we provide our own backward step that simplifies computations
+# to ensure the gradient can be computed.
+class LogNormalCDF(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, input, mu, sigma):
+        """Compute the log-normal cdf based on x and a tensor of mu/sigma"""
+        ctx.save_for_backward(input, mu, sigma)
+        result = torch.where(
+            input > 0,
+            (0.5
+            * (1
+                + torch.erf(
+                    (torch.log(input)
+                    - torch.log(mu)) 
+                    / (torch.log(sigma)
+                    * math.sqrt(2.0))))),
+            0)
+        return result
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        x, mu, sigma = ctx.saved_tensors
+        dx = torch.where(
+            x > 0,
+            (1
             / (x
+                * torch.log(sigma)
+                * (math.sqrt(2.0*math.pi)))
+                * torch.exp(
+                    - ((torch.log(x)-torch.log(mu))**2)
+                    / (2*torch.log(sigma)**2))),
+            0)
+        grad_x = grad_output * dx
+        grad_mu = None
+        grad_sigma = torch.where(
+            (x > 0) & (mu > 0),
+            (math.sqrt(2*math.pi)
+             * torch.exp(
+                 - (((torch.log(x) - torch.log(mu))
+                    / (math.sqrt(2)*torch.log(sigma)))**2))
+             * ((torch.log(mu) - torch.log(x))
+                / (torch.log(sigma)**2 * sigma))),
+            0)
+        if torch.any(torch.isinf(grad_sigma)) or torch.any(torch.isnan(grad_sigma)):
+            print(grad_sigma)
+            print(x)
+            print(mu)
+            print(sigma)
+            raise Exception()
+        return grad_x, grad_mu, grad_sigma
+
+def LnormCDF(x, mu, sigma):
+    result = LogNormalCDF.apply(x, mu, sigma)
+    return result
+
+def LnormPDF(x, mu, sigma):
+    result = torch.where(
+        x > 0,
+        (1
+        / (x
             * torch.log(sigma)
             * (math.sqrt(2.0*math.pi)))
             * torch.exp(
                 - ((torch.log(x)-torch.log(mu))**2)
-                / (2*torch.log(sigma)**2)))
-    return dist
-
-def LnormCDF(x, mu, sigma):
-    """Compute the log-normal cdf based on x and a tensor of mu/sigma"""
-    dist = torch.where(
-        x > 0,
-        (0.5
-         * (1
-            + torch.erf(
-                (torch.log(x)
-                 - torch.log(mu)) 
-                / (torch.log(sigma)
-                   * math.sqrt(2.0))))),
+                / (2*torch.log(sigma)**2))),
         0)
-    return dist
+    return result
+
 
 def validate(tensor, mu):
     return torch.where(
